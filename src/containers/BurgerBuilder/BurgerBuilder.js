ff2bb9f58bcbac3121dfc3679cc67fa3bcdd8842
Daniel Dias <danieldias51@gmail.com>
2018-07-02 15:05:41 +0100

    Adds order button with function to handle if the burger is orderable

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,8 +20,22 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
     };
+
+
+    updatePurchaseState (ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(ingKey => {
+                return ingredients[ingKey]
+            })
+            .reduce((sum, ele) => {
+                return sum + ele
+            }, 0)
+        ;
+        this.setState({purchasable: sum > 0});
+    }
 
     addIngredientHandler = (type) => {
 
@@ -37,7 +51,7 @@ class BurgerBuilder extends Component {
         const newPrice = oldPrice + priceAddition;
 
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-
+        this.updatePurchaseState(updatedIngredients);
     };
 
     removeIngredientHandler = (type) => {
@@ -56,6 +70,7 @@ class BurgerBuilder extends Component {
         const newPrice = oldPrice - priceAddition;
 
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     };
 
     render() {
@@ -72,6 +87,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
+                    purchasable={this.state.purchasable}
                     price={this.state.totalPrice}
                 />
             </Aux>
